b3275f94288142db52d88507a1651c2ccab2b229
p1y1m <155181108+p1y1m@users.noreply.github.com>
2025-09-26 18:56:53 -0300

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,27 @@
+# Definir variable para ubicación
+variable "removed-for-safety" {
+  description = "removed-for-safety"
+  type        = string
+  default     = "removed-for-safety"
+}
+
+# Definir variable para nombre del grupo de recursos
+variable "removed-for-safety" {
+  description = "removed-for-safety"
+  type        = string
+  default     = "removed-for-safety"
+}
+
+# Definir variable para usuario administrador SQL
+variable "removed-for-safety" {
+  description = "removed-for-safety"
+  type        = string
+  default     = "removed-for-safety"
+}
+
+# Definir variable para contraseña administrador SQL
+variable "removed-for-safety" {
+  description = "removed-for-safety"
+  type        = string
+  default     = "removed-for-safety"
+}
